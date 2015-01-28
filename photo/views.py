cc70449eebe869d6a9aaec572267ca332cf00b31
itsMagondu <magonduNjenga@gmail.com>
2015-01-28 14:38:12 +0300

    Added views to photo

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.conf import settings
 
 # Create your views here.
+#@login_required
+def Add(request):
+    args = {}
+    args['base_url'] = settings.BASE_URL
+    args['media_url'] = settings.MEDIA_URL
+    return render_to_response("upload.html", args)
