04f92dc7b6b634d322bd57a4bbc8d8e26c2fc626
itsMagondu <magonduNjenga@gmail.com>
2015-02-05 21:45:02 +0300

    Added django tags on html

--- photo/urls.py
+++ photo/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^add/', 'photo.views.Add'),
     url(r'^admin/', 'photo.views.Admin'),
+    url(r'^upload/', 'photo.views.upload'),
+    url(r'^profile/', 'photo.views.profile'),
 
 )
--- photo/views.py
+++ photo/views.py
@@ -18,3 +18,10 @@ def Admin(request):
 
 def upload(request):
     pass
+
+def profile(request):
+    args = {}
+    args['base_url'] = settings.BASE_URL
+    args['media_url'] = settings.MEDIA_URL
+    return render_to_response("sellerProfile.html", args)
+
