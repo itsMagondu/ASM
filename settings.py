08c782eae5a19202987c5f26c61aaf34ae591bfd
itsMagondu <MagonduNjenga@gmail.com>
2015-03-18 20:52:23 +0000

    Changes on production

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'asm',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'bitnami',                  # Not used with sqlite3.
+        'PASSWORD': 'laFKavm6WVji',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
